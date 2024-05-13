4f0ddfbaf7dbdb082132c79b905fd0ab14ee48ee
fujimuri <alexandr.babich@mail.huji.ac.il>
2024-05-13 18:48:16 +0300

    add get request on select question list Added a get request from HTMX client to Flask server. When selecting a question list from the select menu on main page, we send a get request to our server and receive an HTML of a list item - just a small example for now - and display it in a list below the select menu. The request is sent via HTMX, in the select element itself. Which is cool!

--- server/src/hello.py
+++ server/src/hello.py
@@ -1,7 +1,27 @@
 from flask import Flask
+from flask import request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+# allow cors for all domains on all routers
+# can allow specific cors later:
+# https://flask-cors.readthedocs.io/en/3.0.10/
+
 
 @app.route('/')
 def hello():
 	return 'meow'
+
+@app.route('/question-lists', methods=['GET'])
+def get_question_list():
+	# based on list name, generate an HTML file
+	# with list items with each question's data
+    list_name = request.args.get('question-list-select')
+    if list_name == 'neetcode-150':
+         return '<li>3Sum</li>'
+    else:
+         return '<li>Sudoku</li>'
+
+if __name__ == '__main__':
+    app.run(debug=True)
