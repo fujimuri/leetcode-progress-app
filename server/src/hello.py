9f0fef28a3d10cffb00177c48e4a85ef3cfc1f4c
fujimuri <alexandr.babich@mail.huji.ac.il>
2024-05-16 09:32:24 +0300

    Setup SQLAlchemy on backend

--- server/src/hello.py
+++ server/src/hello.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import request
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 CORS(app)
@@ -8,6 +9,8 @@ CORS(app)
 # can allow specific cors later:
 # https://flask-cors.readthedocs.io/en/3.0.10/
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello():
@@ -19,9 +22,19 @@ def get_question_list():
 	# with list items with each question's data
     list_name = request.args.get('question-list-select')
     if list_name == 'neetcode-150':
-         return '<li>3Sum</li>'
+         return '<li><input type="checkbox">3Sum</li>'
     else:
-         return '<li>Sudoku</li>'
+         return '<li><input type="checkbox">Sudoku</li>'
+    
+def createHTMLForQuestionList(question_list):
+     # this function receives a list of questions
+     # and creates the appropriate HTML where
+     # each question becomes a <li> checkbox item
+     # which has the question name, link, and
+     # difficulty level
+     # the question_list will be received from
+     # the database. i think SQLite will do for now.
+     return
 
 if __name__ == '__main__':
     app.run(debug=True)
